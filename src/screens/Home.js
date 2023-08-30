b591bcde618a56ca8a14e6c4f648cf49c036f4bb
HarshaSenarathna <hsenarathna1999@gmail.com>
2023-08-30 22:02:44 +0530

    homepage partially done

--- src/screens/Home.js
+++ src/screens/Home.js
@@ -1,13 +1,32 @@
 import NavBar from './components/NavBar';
-
+import IntroLogo from '../assets/images/Homepage_click_logo.png';
+import { Fragment } from 'react';
 const Home = () => {
   return (
-    <div>
+    <Fragment>
       <NavBar />
       <div className="homepage">
-        <div className="homepage_hero"></div>
+        <div className="homepage_hero">
+          <div className="homepage_hero_bottom">
+            <h1>Restaurant Name</h1>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Excepturi
+              quidem quis qu
+            </p>
+          </div>
+        </div>
+        <div className="homepage_brandname">
+          <div className="homepage_brandname_logo">
+            <img src={IntroLogo} alt="click image" srcset="" />
+          </div>
+          <p>Seamlessly Connect, Simplify, and Elevate Your Experience!</p>
+        </div>
+        <div className="homepage_info">
+          <div className="homepage_info_cards"></div>
+          <div className="homepage_info_cards"></div>
+        </div>
       </div>
-    </div>
+    </Fragment>
   );
 };
 
