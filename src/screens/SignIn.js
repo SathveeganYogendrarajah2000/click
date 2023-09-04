7fc29086101d7f6fb3003ebe83fbbefcabc499ee
Sathveegan <sathve@gmail.com>
2023-09-05 01:36:00 +0530

    initialize login page

--- src/screens/SignIn.js
+++ src/screens/SignIn.js
@@ -1,17 +1,108 @@
-import NavBar from './components/NavBar';
+import NavBar from "./components/NavBar";
+import Logo from "../assets/Logo.svg";
+import Google from "../assets/google.svg";
+
 const SignIn = () => {
   return (
     <div>
       <NavBar />
-      <div className="sign-in">
-        <h2>Sign In</h2>
-        <form>
-          <label>Email:</label>
-          <input type="email" required />
-          <label>Password:</label>
-          <input type="password" required />
-          <button>Sign In</button>
-        </form>
+      <div className="container">
+        <header className="container_header">
+          <img className="container_header_logo" alt="Logo" src={Logo} />
+          <div className="container_header_heading">
+            <p className="container_header_heading_text">
+              Log in to your account
+            </p>
+            <div className="container_text-wrapper container_header_heading_text-wrapper">
+              <p className="container_header_heading_text-wrapper_div">
+                Welcome back! Please enter your details.
+              </p>
+            </div>
+          </div>
+        </header>
+        <div className="container_content-2">
+          <div className="container_content-2_form">
+            <div className="container_content-2_form_form-item">
+              <div className="container_content-2_form_label">
+                <div className="container_text-wrapper">
+                  <div className="container_content-2_form_text-2">Email</div>
+                </div>
+              </div>
+              <div className="container_content-2_form_form-item_input">
+                <input
+                  className="container_content-2_form_form-item_input_input-base"
+                  type="email"
+                  required
+                />
+              </div>
+            </div>
+            <div className="container_content-2_form_form-item-2">
+              <div className="container_content-2_form_label">
+                <div className="container_text-wrapper">
+                  <div className="container_text-wrapper_text-2">Password</div>
+                </div>
+              </div>
+              <div className="container_content-2_form_form-item-2_password">
+                <input
+                  className="container_content-2_form_form-item-2_password_input-2"
+                  type="password"
+                  required
+                />
+              </div>
+            </div>
+          </div>
+          <div className="container_content-2_row">
+            <div className="container_content-2_row_components-checkbox">
+              <div className="container_content-2_row_components-checkbox_wrapper">
+                <div className="container_content-2_row_components-checkbox_wrapper_checkbox-input">
+                  <input
+                    type="checkbox"
+                    className="container_content-2_row_components-checkbox_wrapper_checkbox-input_bg"
+                  />
+                </div>
+              </div>
+              <div className="container_text-wrapper">
+                <div className="container_text-wrapper_text-2">Remember me</div>
+              </div>
+            </div>
+            <div className="container_text-wrapper">
+              <div className="container_text-wrapper_text-3">
+                Forgot password
+              </div>
+            </div>
+          </div>
+          <div className="container_content-2_actions">
+            <button className="container_content-2_actions_button">
+              {/* <div className="container_text-wrapper">
+                <div className="text-4"> */}
+              Sign in
+              {/* </div>
+              </div> */}
+            </button>
+            <button className="button-medium">
+              <div className="icon">
+                <img className="icon_img" alt="Icon" src={Google} />
+              </div>
+              <div className="container_text-wrapper">
+                <div className="container_text-wrapper_text-5">
+                  Sign in with Google
+                </div>
+              </div>
+            </button>
+          </div>
+        </div>
+        <div className="container_row-2">
+          <div className="container_text-wrapper">
+            <div className="container_text-wrapper_div">
+              Don't have an account?
+            </div>
+          </div>
+          <button>
+            <div className="container_text-wrapper">
+              <div className="container_text-wrapper_text-6">Sign up</div>
+            </div>
+          </button>
+        </div>
       </div>
     </div>
   );
