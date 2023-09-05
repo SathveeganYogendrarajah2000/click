655de6a520070193a68a07bb9947052d921a49c8
Sathveegan <sathve@gmail.com>
2023-09-06 01:34:37 +0530

    signin page done except auth settings

--- src/screens/SignIn.js
+++ src/screens/SignIn.js
@@ -1,8 +1,48 @@
-import NavBar from "./components/NavBar";
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
+// import "firebase/auth"; // for google sign in // TODO:
+
 import Logo from "../assets/Logo.svg";
 import Google from "../assets/google.svg";
 
 const SignIn = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const navigate = useNavigate();
+
+  // TODO: implement firebase signin
+  const handleSignIn = async () => {
+    try {
+      // await firebase.auth().signInWithEmailAndPassword(email, password);
+      // Sign-in successful, you can redirect to the home page or perform other actions.
+      console.log("Sign-in successful");
+      navigate("/");
+    } catch (error) {
+      console.error("Error signing in:", error.message);
+    }
+  };
+
+  // TODO: implement firebase google signin
+  const handleGoogleSignIn = async () => {
+    try {
+      // const provider = new firebase.auth.GoogleAuthProvider();
+      // await firebase.auth().signInWithPopup(provider);
+      // Sign-in successful, you can redirect to the home page or perform other actions.
+      console.log("Google Sign-in successful");
+    } catch (error) {
+      console.error("Error signing in with Google:", error.message);
+    }
+  };
+
+  const handleSignUp = async () => {
+    try {
+      navigate("/signup");
+    } catch (error) {
+      console.error("Error signing up:", error.message);
+    }
+  };
+
   return (
     <div className="frame">
       <div className="container">
@@ -19,7 +59,7 @@ const SignIn = () => {
             </div>
           </div>
         </header>
-        <div className="container_content-2">
+        <form className="container_content-2">
           <div className="container_content-2_form">
             <div className="container_content-2_form_form-item">
               <div className="container_content-2_form_label">
@@ -37,6 +77,8 @@ const SignIn = () => {
                   id="email"
                   className="container_content-2_form_form-item_input_input-base"
                   type="email"
+                  value={email}
+                  onChange={(e) => setEmail(e.target.value)}
                   required
                 />
               </div>
@@ -57,6 +99,8 @@ const SignIn = () => {
                   id="password"
                   className="container_content-2_form_form-item-2_password_input-2"
                   type="password"
+                  value={password}
+                  onChange={(e) => setPassword(e.target.value)}
                   required
                 />
               </div>
@@ -89,14 +133,20 @@ const SignIn = () => {
             </div>
           </div>
           <div className="container_content-2_actions">
-            <button className="container_content-2_actions_button container_text-wrapper">
+            <button
+              className="container_content-2_actions_button container_text-wrapper"
+              onSubmit={handleSignIn}
+            >
               {/* <div className="container_text-wrapper">
                 <div className="text-4"> */}
               Sign in
               {/* </div>
               </div> */}
             </button>
-            <button className="container_content-2_actions_button-medium">
+            <button
+              className="container_content-2_actions_button-medium"
+              onSubmit={handleGoogleSignIn}
+            >
               <div className="container_content-2_actions_button-medium_icon">
                 <img
                   className="container_content-2_actions_button-medium_icon_img"
@@ -111,7 +161,7 @@ const SignIn = () => {
               </div>
             </button>
           </div>
-        </div>
+        </form>
         <div className="container_row-2">
           <div className="container_text-wrapper">
             <div className="container_text-wrapper_div">
@@ -119,7 +169,7 @@ const SignIn = () => {
             </div>
           </div>
 
-          <div className="container_text-wrapper">
+          <div className="container_text-wrapper" onClick={handleSignUp}>
             <div className="container_text-wrapper_text-6">Sign up</div>
           </div>
         </div>
