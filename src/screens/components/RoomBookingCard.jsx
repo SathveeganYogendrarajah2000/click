53bb15ae6537ab5659d1c3d1a780849baf5a782d
Sathveegan <sathve@gmail.com>
2023-09-12 01:22:23 +0530

    finishing up room booking card

--- src/screens/components/RoomBookingCard.jsx
+++ src/screens/components/RoomBookingCard.jsx
@@ -7,79 +7,59 @@ const RoomBookingCard = (props) => {
       <div className="roombookingcardContainer_image">
         <img
           className="roombookingcardContainer_image_picture-web"
-          alt="Room image"
+          alt="Room"
           src={props.roomImage}
         />
       </div>
       <div className="roombookingcardContainer_details">
         <div className="roombookingcardContainer_details_c1">
-          <div className="heading-luxury">Luxury Room Facade View</div>
-          <div className="p">
-            <p className="elegant-and-warm">
-              Elegant and warm color palettes, combined with textured
-              furnishings, give the rooms a sophisticated style that feels
-              personally inviting and cozy
-            </p>
+          <div className="roombookingcardContainer_details_c1_heading">
+            {props.name}
           </div>
-          <div className="div-row">
-            <div className="overlap-group">
-              <div className="div-col-2">
-                <div className="div-image-container">
-                  <img className="img" alt="Bedtype png" src={bedType} />
-                </div>
-                <div className="div-wrapper">
-                  <div className="text-wrapper-3">KING/TWIN</div>
-                </div>
+          <p className="roombookingcardContainer_details_c1_description">
+            {props.description}
+          </p>
+          <div className="roombookingcardContainer_details_c1_row1">
+            <div className="roombookingcardContainer_details_c1_row1_feature">
+              <img
+                className="roombookingcardContainer_details_c1_row1_feature_img"
+                alt="Bedtype png"
+                src={bedType}
+              />
+              <div className="roombookingcardContainer_details_c1_row1_feature_text">
+                {props.type}
               </div>
             </div>
-            <div className="div-col-wrapper">
-              <div className="div-col-3">
-                <div className="div-image-container">
-                  <img
-                    className="img"
-                    alt="Maximumoccoumpany"
-                    src={maximumOccoumpany}
-                  />
-                </div>
-                <div className="span-room-selling">
-                  <div className="text-wrapper-3">Up to 4 guests</div>
-                </div>
+            <div className="roombookingcardContainer_details_c1_row1_feature">
+              <img
+                className="roombookingcardContainer_details_c1_row1_feature_img"
+                alt="Maximumoccoumpany"
+                src={maximumOccoumpany}
+              />
+              <div className="roombookingcardContainer_details_c1_row1_feature_text">
+                Up to {props.capacity} guests
               </div>
             </div>
-            <div className="div-room-property-wrapper">
-              <div className="div-room-property">
-                <div className="star-png" />
-                <p className="text-wrapper-3">
-                  Double glazed windows to ensure its sound proof
-                </p>
-              </div>
-            </div>
+          </div>
+          <div className="roombookingcardContainer_details_c1_row2">
+            Double glazed windows to ensure its sound proof
           </div>
         </div>
         <div className="div-room-rate-wrap roombookingcardContainer_details_c2">
-          <div className="overlap">
-            <div className="div-rate-card-actual">
-              <div className="text-wrapper-4">$</div>
-              <div className="text-wrapper-5">125</div>
-              <div className="text-wrapper-6">*</div>
+          <div className="roombookingcardContainer_details_c2_row1">
+            <div className="roombookingcardContainer_details_c2_row1_01">
+              $ {props.price}*
             </div>
-            <div className="text-wrapper-7">Starting Rate/Night</div>
-          </div>
-          <div className="div-rate-card-2">
-            <div className="last-room-svg">
-              <div className="group-wrapper">
-                <div className="group">
-                  <div className="text-wrapper-8">Last few rooms available</div>
-                  <img className="vector" alt="Vector" src="vector.svg" />
-                </div>
-              </div>
+            <div className="roombookingcardContainer_details_c2_row1_02">
+              Starting Rate/Night
             </div>
-            <div className="text-wrapper-9">Last few rooms available</div>
           </div>
-          <button className="button-2">
-            <div className="text-wrapper-10">VIEW RATES</div>
-            <img className="icon-2" alt="Icon" src="image.svg" />
-          </button>
+
+          <div className="roombookingcardContainer_details_c2_row2">
+            <button className="button-2 roombookingcardContainer_details_c2_row2_btn">
+              VIEW RATES
+            </button>
+          </div>
         </div>
       </div>
     </div>
