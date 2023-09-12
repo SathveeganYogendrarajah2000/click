ddce000dc99c1601e07b42d7d6c3b3454ebfa478
Sathveegan <sathve@gmail.com>
2023-09-12 15:49:33 +0530

    initialized room card for home page

--- src/screens/Home.js
+++ src/screens/Home.js
@@ -1,13 +1,100 @@
-import NavBar from './components/NavBar';
-import Footer from './components/Footer';
-import IntroLogo from '../assets/images/Homepage_click_logo.png';
-import { Fragment } from 'react';
+import NavBar from "./components/NavBar";
+import Footer from "./components/Footer";
+import IntroLogo from "../assets/images/Homepage_click_logo.png";
+import { Fragment, useState } from "react";
+import NavBarStyled from "./components/NavBarStyled";
+import RoomCard from "./components/RoomCard";
 const Home = () => {
+  const [roomType, setRoomType] = useState("");
+  const [checkInDate, setCheckInDate] = useState(null);
+  const [checkOutDate, setCheckOutDate] = useState(null);
+  const [adults, setAdults] = useState(1);
+  const [children, setChildren] = useState(0);
+
+  const handleSearch = () => {
+    // Handle the search logic here
+  };
   return (
     <Fragment>
-      <NavBar />
       <div className="homepage">
-        <div className="homepage_hero">
+        <div className="homepage_hero bookingContainer_hero">
+          <NavBarStyled />
+          <div className="bookingContainer_hero_searchbar">
+            <select
+              className="bookingContainer_hero_searchbar_select-room"
+              value={roomType}
+              onChange={(e) => setRoomType(e.target.value)}
+            >
+              <option value="">Select Room Type</option>
+              <option value="suite">Suite Room</option>
+              <option value="single">Single Bedroom</option>
+              <option value="double">Double Bedroom</option>
+              <option value="double">KING/TWIN</option>
+              {/* Add more room types */}
+            </select>
+            <div className="bookingContainer_hero_searchbar_date-picker-section">
+              <div className="bookingContainer_hero_searchbar_date-picker-section-01">
+                <label
+                  className="bookingContainer_hero_searchbar_date-picker-section_label"
+                  htmlFor="date-picker-1"
+                >
+                  Check-in
+                </label>
+                <input
+                  id="date-picker-1"
+                  className="bookingContainer_hero_searchbar_date-picker-section_picker"
+                  type="date"
+                  value={checkInDate}
+                  onChange={(date) => setCheckInDate(date)}
+                />
+              </div>
+              <div className="bookingContainer_hero_searchbar_date-picker-section-01">
+                <label
+                  className="bookingContainer_hero_searchbar_date-picker-section_label"
+                  htmlFor="date-picker-2"
+                >
+                  Check-out
+                </label>
+                <input
+                  id="date-picker-2"
+                  className="bookingContainer_hero_searchbar_date-picker-section_picker"
+                  type="date"
+                  value={checkOutDate}
+                  onChange={(date) => setCheckOutDate(date)}
+                />
+              </div>
+            </div>
+
+            <div className="bookingContainer_hero_searchbar_people-section">
+              <div className="bookingContainer_hero_searchbar_people-section-item">
+                <label style={{ color: "#fff" }}>Adults:</label>
+                <input
+                  className="bookingContainer_hero_searchbar_people-section_input-people"
+                  type="number"
+                  value={adults}
+                  onChange={(e) => setAdults(e.target.value)}
+                />
+              </div>
+              <div className="bookingContainer_hero_searchbar_people-section-item">
+                <label style={{ color: "#fff" }}>Children:</label>
+                <input
+                  className="bookingContainer_hero_searchbar_people-section_input-people"
+                  type="number"
+                  value={children}
+                  onChange={(e) => setChildren(e.target.value)}
+                />
+              </div>
+            </div>
+
+            <button
+              className="bookingContainer_hero_searchbar_check-button"
+              onClick={handleSearch}
+            >
+              Check Availability
+            </button>
+          </div>
+        </div>
+        {/* <div className="homepage_hero">
           <div className="homepage_hero_bottom">
             <h1>Restaurant Name</h1>
             <p>
@@ -15,7 +102,7 @@ const Home = () => {
               quidem quis qu
             </p>
           </div>
-        </div>
+        </div> */}
         <div className="homepage_brandname">
           <div className="homepage_brandname_logo">
             {/* <img src={IntroLogo} alt="click image" srcset="" /> */}
@@ -24,7 +111,9 @@ const Home = () => {
           <p>Seamlessly Connect, Simplify, and Elevate Your Experience!</p>
         </div>
         <div className="homepage_info">
-          <div className="homepage_info_cards"></div>
+          <div className="homepage_info_cards">
+            <RoomCard />
+          </div>
           <div className="homepage_info_cards"></div>
         </div>
       </div>
