22c3f6ef3722329bd9ea6b2916826b191027f01a
Sathveegan <sathve@gmail.com>
2023-09-13 19:53:08 +0530

    implement firestore in standard rates component

--- src/screens/subScreens/StandardRates.jsx
+++ src/screens/subScreens/StandardRates.jsx
@@ -1,83 +1,34 @@
 import RoomBookingCard from "../components/RoomBookingCard";
 import { db } from "../../firebase";
-import {
-  collection,
-  doc,
-  documentId,
-  getDoc,
-  getDocs,
-  query,
-  where,
-} from "firebase/firestore";
+import { collection, getDocs } from "firebase/firestore";
+import { useEffect, useState } from "react";
 
 const StandardRates = () => {
-  // const roomRef = collection(db, "rooms");
-  // const q = query(roomRef, where("name", "==", "Luxury Room Facade View"));
-  // const querySnapshot = await getDocs(q);
-  // querySnapshot.forEach((doc) => {
-  //   // doc.data() is never undefined for query doc snapshots
-  //   console.log(doc.id, " => ", doc.data());
-  // });
+  const [roomsDetails, setRoomsDetails] = useState([]); // Initialize state to hold data
 
-  // const docRef = doc(db, "rooms", "R001");
-  // const docSnap = await getDoc(docRef);
+  useEffect(() => {
+    // Use an asynchronous function inside useEffect to fetch data
+    const fetchData = async () => {
+      try {
+        const querySnapshot = await getDocs(collection(db, "rooms"));
+        const roomData = [];
+        querySnapshot.forEach((doc) => {
+          roomData.push(doc.data());
+        });
+        setRoomsDetails(roomData); // Set the data in state when fetched
+      } catch (error) {
+        console.error("Error fetching data:", error);
+      }
+    };
 
-  // if (docSnap.exists()) {
-  //   console.log(docSnap.data());
-  // } else {
-  //   // docSnap.data() will be undefined in this case
-  //   console.log("No such document!");
-  // }
+    fetchData(); // Call the async function to fetch data
 
-  //   try {
-  //     const docsSnap =  getDocs(roomRef);
-  //     if(docsSnap.docs.length > 0) {
-  //        docsSnap.forEach(doc => {
-  //           console.log(doc.data());
-  //           console.log(doc.id);
-  //        })
-  //     }
-  // } catch (error) {
-  //     console.log(error);
-  // }
-
-  const bookingCardData = [
-    {
-      imagePath:
-        "https://images.unsplash.com/photo-1618773928121-c32242e63f39?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1770&q=80",
-      name: "Luxury Room Facade View",
-      description:
-        "Elegant and warm color palettes, combined with textured furnishings, give the rooms a sophisticated style that feels personally inviting and cozy",
-      type: "King/Twin",
-      capacity: "4",
-      price: "125",
-    },
-    {
-      imagePath:
-        "https://images.unsplash.com/photo-1618773928121-c32242e63f39?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1770&q=80",
-      name: "Luxury Room Facade View",
-      description:
-        "Elegant and warm color palettes, combined with textured furnishings, give the rooms a sophisticated style that feels personally inviting and cozy",
-      type: "King/Twin",
-      capacity: "4",
-      price: "125",
-    },
-    {
-      imagePath:
-        "https://images.unsplash.com/photo-1618773928121-c32242e63f39?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1770&q=80",
-      name: "Luxury Room Facade View",
-      description:
-        "Elegant and warm color palettes, combined with textured furnishings, give the rooms a sophisticated style that feels personally inviting and cozy",
-      type: "King/Twin",
-      capacity: "4",
-      price: "125",
-    },
-  ];
-
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []); // Empty dependency array to fetch data once on component mount
   return (
     <div className="guestroomContainer_rooms_standardrates">
       {/* {console.log(querySnapshot.data())} */}
-      {bookingCardData.map((bookingCard, index) => (
+      {roomsDetails.map((bookingCard, index) => (
         <RoomBookingCard
           key={index} // Use a unique key for each card (typically an ID)
           imagePath={bookingCard.imagePath}
