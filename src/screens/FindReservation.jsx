93806791151ab1c36918695ebd8749ff5437ef10
Sathveegan <sathve@gmail.com>
2023-09-14 03:26:07 +0530

    initialize find reservation page

---
+++ src/screens/FindReservation.jsx
@@ -0,0 +1,11 @@
+import NavBar from "./components/NavBar";
+const FindReservation = () => {
+  return (
+    <div>
+      <NavBar />
+      <h1>FindReservation</h1>
+    </div>
+  );
+};
+
+export default FindReservation;
