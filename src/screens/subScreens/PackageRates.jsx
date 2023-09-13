72fdb8390e7d5ae7c6ad89a1bd17daa728ade653
Sathveegan <sathve@gmail.com>
2023-09-13 17:34:58 +0530

    filled sections in booking page

--- src/screens/subScreens/PackageRates.jsx
+++ src/screens/subScreens/PackageRates.jsx
@@ -1,7 +1,51 @@
+import RoomBookingCard from "../components/RoomBookingCard";
+
 const PackageRates = () => {
+  const bookingCardData = [
+    {
+      imagePath:
+        "https://images.unsplash.com/photo-1618773928121-c32242e63f39?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1770&q=80",
+      name: "Luxury Room Facade View",
+      description:
+        "Elegant and warm color palettes, combined with textured furnishings, give the rooms a sophisticated style that feels personally inviting and cozy",
+      type: "King/Twin",
+      capacity: "4",
+      price: "125",
+    },
+    {
+      imagePath:
+        "https://images.unsplash.com/photo-1618773928121-c32242e63f39?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1770&q=80",
+      name: "Luxury Room Facade View",
+      description:
+        "Elegant and warm color palettes, combined with textured furnishings, give the rooms a sophisticated style that feels personally inviting and cozy",
+      type: "King/Twin",
+      capacity: "4",
+      price: "125",
+    },
+    {
+      imagePath:
+        "https://images.unsplash.com/photo-1618773928121-c32242e63f39?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1770&q=80",
+      name: "Luxury Room Facade View",
+      description:
+        "Elegant and warm color palettes, combined with textured furnishings, give the rooms a sophisticated style that feels personally inviting and cozy",
+      type: "King/Twin",
+      capacity: "4",
+      price: "125",
+    },
+  ];
   return (
-    <div>
-      <h1>PackageRates</h1>
+    <div className="guestroomContainer_rooms_standardrates">
+      {bookingCardData.map((bookingCard, index) => (
+        <RoomBookingCard
+          key={index} // Use a unique key for each card (typically an ID)
+          imagePath={bookingCard.imagePath}
+          name={bookingCard.name}
+          description={bookingCard.description}
+          type={bookingCard.type}
+          capacity={bookingCard.capacity}
+          price={bookingCard.price}
+        />
+      ))}
     </div>
   );
 };
