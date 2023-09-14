3b54b6245be3141ce7bc67e3a22ef9330da89641
Sathveegan <sathve@gmail.com>
2023-09-14 15:45:36 +0530

    basic structure of checkout page

--- src/screens/CheckoutPage.jsx
+++ src/screens/CheckoutPage.jsx
@@ -4,8 +4,7 @@ import { db } from "../firebase";
 
 import NavBar from "./components/NavBar";
 import Footer from "./components/Footer";
-import { collection, getDoc, where } from "@firebase/firestore";
-import { query } from "express";
+import { collection, query, where, getDocs } from "@firebase/firestore";
 
 const CheckoutPage = () => {
   const { roomId } = useParams();
@@ -18,15 +17,21 @@ const CheckoutPage = () => {
   const roomQuery = query(roomsRef, where("roomID", "==", roomId));
 
   useEffect(() => {
-    // Use getDoc to fetch the document that matches the query
+    // Use getDocs to fetch documents that match the query
     const fetchRoomData = async () => {
       try {
-        const docSnap = await getDoc(roomQuery);
-        if (docSnap.exists()) {
-          // Document exists, set the data to state
-          setRoomData(docSnap.data());
+        const querySnapshot = await getDocs(roomQuery);
+        if (!querySnapshot.empty) {
+          // Get the first document from the query snapshot
+          const docSnap = querySnapshot.docs[0];
+          if (docSnap.exists()) {
+            // Document exists, set the data to state
+            setRoomData(docSnap.data());
+          } else {
+            console.log("No such document!");
+          }
         } else {
-          console.log("No such document!");
+          console.log("No matching documents!");
         }
       } catch (error) {
         console.error("Error fetching room data:", error);
@@ -58,16 +63,69 @@ const CheckoutPage = () => {
     <>
       <NavBar />
       <div className="checkout-container">
-        <h1>Room Details</h1>
-        <p>Name: {roomDetails.name}</p>
-        <p>Type: {roomDetails.type}</p>
-        <p>Check-in Date: {roomDetails.checkInDate}</p>
-        <p>Check-out Date: {roomDetails.checkOutDate}</p>
-        <p>Adults: {roomDetails.adults}</p>
-        <p>Children: {roomDetails.children}</p>
-        <p>Price per Night: ${roomDetails.pricePerNight}</p>
-        <p>Total Price: ${calculateTotalPrice()}</p>
-        <button className="confirm-button">Confirm Booking</button>
+        <div className="checkout-container_roomDetails">
+          {roomData ? (
+            <>
+              <h1 className="checkout-container_roomDetails_name">
+                {roomData.name}
+              </h1>
+              <p className="checkout-container_roomDetails_price">
+                Price: ${roomData.price} per night
+              </p>
+              <p className="checkout-container_roomDetails_type">
+                Type: {roomData.type}
+              </p>
+              <p className="checkout-container_roomDetails_capacity">
+                Capacity: {roomData.capacity} guests
+              </p>
+              <p className="checkout-container_roomDetails_description">
+                Description: {roomData.description}
+              </p>
+              <img
+                className="checkout-container_roomDetails_img"
+                src={roomData.picturePath}
+                alt={roomData.name}
+              />
+            </>
+          ) : (
+            <p className="checkout-container_roomDetails_loading">
+              Loading room data...
+            </p>
+          )}
+        </div>
+
+        <div className="checkout-container_customerDetails">
+          <h1 className="checkout-container_customerDetails_title">
+            Room Details
+          </h1>
+          <p className="checkout-container_customerDetails_name">
+            Name: {roomDetails.name}
+          </p>
+          <p className="checkout-container_customerDetails_type">
+            Type: {roomDetails.type}
+          </p>
+          <p className="checkout-container_customerDetails_checkIN">
+            Check-in Date: {roomDetails.checkInDate}
+          </p>
+          <p className="checkout-container_customerDetails_checkOut">
+            Check-out Date: {roomDetails.checkOutDate}
+          </p>
+          <p className="checkout-container_customerDetails_adults">
+            Adults: {roomDetails.adults}
+          </p>
+          <p className="checkout-container_customerDetails_children">
+            Children: {roomDetails.children}
+          </p>
+          <p className="checkout-container_customerDetails_price">
+            Price per Night: ${roomDetails.pricePerNight}
+          </p>
+          <p className="checkout-container_customerDetails_totalPrice">
+            Total Price: ${calculateTotalPrice()}
+          </p>
+          <button className="checkout-container_customerDetails_confirm-button">
+            Confirm Booking
+          </button>
+        </div>
       </div>
       <Footer />
     </>
