c8934ae3d9f3987b49448c6e5d3668639b79ac7e
Sathveegan <sathve@gmail.com>
2023-09-15 15:30:43 +0530

    floating button updated

--- src/screens/components/FloatingChatButton.js
+++ src/screens/components/FloatingChatButton.js
@@ -2,15 +2,17 @@ const FloatingChatButton = ({ onClick }) => {
   const buttonStyle = {
     "text-align": "center",
     position: "fixed",
-    width: "350px",
+    width: "100px",
     bottom: "10px", // Adjust as needed for the desired vertical position
     right: "20px", // Adjust as needed for the desired horizontal position
-    background: "#007bff", // Button background color
+    background: "#aa7938", // Button background color
     color: "#fff", // Button text color
     padding: "10px 20px",
     cursor: "pointer",
     boxShadow: "0px 4px 6px rgba(0, 0, 0, 0.1)",
     zIndex: "1000", // Ensure the button is above other content
+    opacity: "0.8",
+    "border-radius": "10px",
   };
 
   return (
