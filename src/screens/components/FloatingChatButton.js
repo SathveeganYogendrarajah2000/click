361112de716eb708bba80d834d4a450478da8b9d
HarshaSenarathna <hsenarathna1999@gmail.com>
2023-09-16 06:25:34 +0530

    Chat bot button styles changed

--- src/screens/components/FloatingChatButton.js
+++ src/screens/components/FloatingChatButton.js
@@ -1,23 +1,28 @@
+import chatLogo from '../../assets/comment-dots-regular.svg';
 const FloatingChatButton = ({ onClick }) => {
   const buttonStyle = {
-    "text-align": "center",
-    position: "fixed",
-    width: "100px",
-    bottom: "10px", // Adjust as needed for the desired vertical position
-    right: "20px", // Adjust as needed for the desired horizontal position
-    background: "#aa7938", // Button background color
-    color: "#fff", // Button text color
-    padding: "10px 20px",
-    cursor: "pointer",
-    boxShadow: "0px 4px 6px rgba(0, 0, 0, 0.1)",
-    zIndex: "1000", // Ensure the button is above other content
-    opacity: "0.8",
-    "border-radius": "10px",
+    'text-align': 'center',
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+    position: 'fixed',
+    width: '60px',
+    height: '60px',
+    bottom: '10px', // Adjust as needed for the desired vertical position
+    right: '20px', // Adjust as needed for the desired horizontal position
+    background: '#aa7938', // Button background color
+    color: '#fff', // Button text color
+    // padding: '10px 20px',
+    cursor: 'pointer',
+    boxShadow: '0px 4px 6px rgba(0, 0, 0, 0.1)',
+    zIndex: '1000', // Ensure the button is above other content
+    opacity: '0.8',
+    'border-radius': '10px',
   };
 
   return (
     <div style={buttonStyle} onClick={onClick}>
-      Chat
+      <img src={chatLogo} alt="Chat bot" width={25} />
     </div>
   );
 };
