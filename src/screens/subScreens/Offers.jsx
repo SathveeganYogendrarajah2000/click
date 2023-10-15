fe4b6772d6e3628ab7a81340c6d1d5cee32032e1
sathu2000 <susathu2000@gmail.com>
2023-10-15 08:19:43 +0530

    offers almost done

--- src/screens/subScreens/Offers.jsx
+++ src/screens/subScreens/Offers.jsx
@@ -10,6 +10,9 @@ import {db} from "../../../src/firebase.js";
 
 function Offers(){
   const [offers, setOffers] = useState([]);
+  //get the current date in YYYY-MM-DD format
+  const today = new Date();
+  const todayFormatted = today.toISOString().split('T')[0];
 
   // Function to get the image URL based on offer type
   function getImageForOfferType(offerType) {
@@ -35,14 +38,17 @@ function Offers(){
         querySnapshot.forEach((doc) => {
           // Extract data from each document
           const offer = doc.data();
-          offerData.push(offer);
+          if(offer.endDate >= todayFormatted){
+            offerData.push(offer);
+          }
+          
         });
         setOffers(offerData);
       })
       .catch((error) => {
         console.error('Error fetching offers: ', error);
       });
-  }, []);
+  }, [todayFormatted]);
 
   return (   
       <div className="offers_contentContainer">
@@ -50,15 +56,9 @@ function Offers(){
         <h2 className="offers_contentHeading2">CLICK at Colombo</h2>
         <p className="offers_contentparagraph"> Discover an array of enticing offers that have been thoughtfully designed to enhance your stay and dining experience at our hotel and restaurant. Whether you're planning a romantic getaway, a family vacation, or a memorable celebration, our exclusive offers cater to all your desires. </p>
         <h3 className="offers_contentHeading3" >Offer details</h3>
-        <p className="offers_contentparagraph">Feast like royalty at our weekend brunch at Shang Palace which 
-        promises a vibrant selection of unimited 
-        authentic Chinese dishes featuring intricate delicacies 
-        including the finest dim sums, variety of meats and 
-        seafood along with a tempting choice of rice and noodles.</p>
-        <p className="offers_contentparagraph"> <b>Family Fun : </b>Treat your family to a well-deserved break. Our family-friendly offers include spacious accommodations, activities for all ages, and dining options that cater to even the pickiest eaters.</p>
-        <p className="offers_contentparagraph"> <b>Special Occasions : </b> Make your special occasions truly extraordinary. Whether it's a birthday, anniversary, or any other milestone, our celebration packages ensure your event is celebrated in style.</p>
-        <p className="offers_contentparagraph">The food may also be paired with a variety of Chinese spirits and cocktails especially for the Yum Cha brunch Yum Cha is served every Saturday, Sunday and on all Public Holidays from 12.00pm till 3.00pm</p>
-        <p className="offers_contentparagraph">Contact us at(94) 788 8288 for reservations or more information.</p>
+        <p className="offers_contentparagraph">Feast like royalty at our weekend brunch at Shang Palace which promises a vibrant selection of unimited 
+        authentic Chinese dishes featuring intricate delicacies including the finest dim sums, variety of meats and seafood along with a tempting choice of rice and noodles.<br></br><br></br>
+        <b>Family Fun : </b>Treat your family to a well-deserved break. Our family-friendly offers include spacious accommodations, activities for all ages, and dining options that cater to even the pickiest eaters.<br></br><b>Special Occasions : </b> Make your special occasions truly extraordinary. Whether it's a birthday, anniversary, or any other milestone, our celebration packages ensure your event is celebrated in style. <br></br><br></br>The food may also be paired with a variety of Chinese spirits and cocktails especially for the Yum Cha brunch Yum Cha is served every Saturday, Sunday and on all Public Holidays from 12.00pm till 3.00pm. <br></br><br></br>Contact us at(94) 788 8288 for reservations or more information.</p>
         <p className="offers_contentparagraph">These offers are available for a limited time, so be sure to check back frequently for new promotions and deals. We're committed to making your stay with us as memorable as possible, and our exclusive offers are designed to provide you with exceptional value and unparalleled experiences.
         Visit our Offers page to explore the full range of packages and promotions. We look forward to welcoming you to our hotel and restaurant, where every offer is a gateway to a world of luxury, adventure, and indulgence.</p>
         <h3 className="offers_contentHeading3">Recommended Offers</h3>
@@ -69,8 +69,8 @@ function Offers(){
               <img src={getImageForOfferType(offer.on)} alt={offer.on} style={{width:"100px",height:"100px",borderRadius:"50%",boxShadow :" 0 0 5px rgba(0, 0, 0, 0.5)"}}  />
               {/* Display offer details */}
               <p className="offers_details"> {offer.on} offer   </p>
-              <p className="offers_details"> type : {offer.type}  </p>
-              <p className="offers_details">  {offer.startDate} to  {offer.endDate} </p>
+              <p className="offers_details"> Type : {offer.type}  </p>
+              <p className="offers_details"> Offer Period <br></br> <br></br> {offer.startDate} to  {offer.endDate} </p>
               <p className="offers_details"> on {offer.percentage}% </p>
               
             </div>
